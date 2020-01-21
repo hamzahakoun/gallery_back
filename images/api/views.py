1ceb786f8e73f6d4840aa5e7021c502625d530a3
hakounh94 <hakoun.h.94@gmail.com>
2020-01-21 23:06:47 +0300

    ddd

--- images/api/views.py
+++ images/api/views.py
@@ -9,7 +9,7 @@ from django.contrib.contenttypes.models import ContentType
 from events.models import Event
 from .permissions import IsUserOwnerOrReadOnly
 
-###sdfsdfsdfsdfsdfsdfsdfad
+###sdfsdfsdfsdfsdfsdfsdfadfddfgdfg
 
 def get_images_on_tags(tags_list) :
     q = Q()
